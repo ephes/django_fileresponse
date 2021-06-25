580fef96ed568a222755b89db0e54b4bb0470997
Jochen Wersdörfer <jochen@wersdoerfer.de>
2021-06-25 12:48:29 +0200

    docs for http module

--- fileresponse/_nbdev.py
+++ fileresponse/_nbdev.py
@@ -3,19 +3,19 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"AsyncFileASGIHandler": "01_handlers.ipynb",
-         "AsyncResponse": "01_http.ipynb",
-         "AiofileFileResponse": "01_http.ipynb",
-         "AiobotocoreFileResponse": "01_http.ipynb",
          "get_config": "01_scripts.ipynb",
          "patch_config": "01_scripts.ipynb",
          "patch_lib_name": "01_scripts.ipynb",
          "nbdev_build_docs_patched": "01_scripts.ipynb",
-         "get_asgi_application": "02_asgi.ipynb"}
+         "get_asgi_application": "02_asgi.ipynb",
+         "AsyncResponse": "02_http.ipynb",
+         "AiofileFileResponse": "02_http.ipynb",
+         "AiobotocoreFileResponse": "02_http.ipynb"}
 
 modules = ["handlers.py",
-           "http.py",
            "scripts.py",
-           "asgi.py"]
+           "asgi.py",
+           "http.py"]
 
 doc_url = "https://ephes.github.io/django_fileresponse/"
 
